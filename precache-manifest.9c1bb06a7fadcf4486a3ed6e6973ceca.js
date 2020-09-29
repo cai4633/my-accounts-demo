03ff04dab9fec58e06404824b644e9e37c2dbc89
cai4633 <463353522@qq.com>
2020-09-29 15:53:21 +0800

    v1.x

---
+++ precache-manifest.9c1bb06a7fadcf4486a3ed6e6973ceca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f3ded717bf07bea3575ed78019fc98",
+    "url": "/my-accounts-demo/index.html"
+  },
+  {
+    "revision": "ad245a81427a7c43e62f",
+    "url": "/my-accounts-demo/static/css/2.1a430bd9.chunk.css"
+  },
+  {
+    "revision": "9fa61f41f737009ddd41",
+    "url": "/my-accounts-demo/static/css/main.d59cf3e1.chunk.css"
+  },
+  {
+    "revision": "ad245a81427a7c43e62f",
+    "url": "/my-accounts-demo/static/js/2.3b86bdfd.chunk.js"
+  },
+  {
+    "revision": "235c630b9dc8eaa5debfdadd00b80e01",
+    "url": "/my-accounts-demo/static/js/2.3b86bdfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa61f41f737009ddd41",
+    "url": "/my-accounts-demo/static/js/main.7ea367cd.chunk.js"
+  },
+  {
+    "revision": "05acc636705ca18d8f8e",
+    "url": "/my-accounts-demo/static/js/runtime-main.4816409c.js"
+  }
+]);
